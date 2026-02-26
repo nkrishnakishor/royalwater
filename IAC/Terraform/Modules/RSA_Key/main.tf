fdc4dd605bdc84fdb7df850cf7d059432e50ebd8
Krishna <nkrishnakishor@gmail.com>
2026-02-26 22:01:18 +0800

    fixed TF formating issue
    Signed-off-by: Krishna <nkrishnakishor@gmail.com>

--- IAC/Terraform/Modules/RSA_Key/main.tf
+++ IAC/Terraform/Modules/RSA_Key/main.tf
@@ -4,7 +4,7 @@ resource "tls_private_key" "this" {
 }
 
 resource "local_sensitive_file" "private_key" {
-  count           = var.save_file ? 1 : 0
+  count = var.save_file ? 1 : 0
 
   filename        = "${var.output_path}/${var.key_name}.pem"
   content         = tls_private_key.this.private_key_pem
@@ -12,7 +12,7 @@ resource "local_sensitive_file" "private_key" {
 }
 
 resource "local_file" "public_key" {
-  count           = var.save_file ? 1 : 0
+  count = var.save_file ? 1 : 0
 
   filename        = "${var.output_path}/${var.key_name}.pub"
   content         = tls_private_key.this.public_key_pem
